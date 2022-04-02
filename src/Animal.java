db53213a968fa4d5377a8905c5ca378586f69592
Omar Bautista <joxebus@gmail.com>
2022-04-02 01:01:47 -0600

    Modify Animal class by adding toString()

--- src/Animal.java
+++ src/Animal.java
@@ -3,4 +3,8 @@ public abstract class Animal {
 
     public abstract void makeSound();
     public abstract void sayName();
+
+    public String toString() {
+        return "This is an Animal";
+    }
 }
